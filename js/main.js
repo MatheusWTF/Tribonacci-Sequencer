2ed21c91c52fe7d7c9aa7e880067a5744811a551
Matheus Diniz Rodrigues <mdrwtf@gmail.com>
2017-12-10 15:08:42 -0200

    Tribonacci Sequencer App (Minimal style)

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const number = document.getElementById('number');
+const submit = document.getElementById('submit');
+const output = document.getElementById('outp');
+
+
+function fibonacci(){
+    let iteration = number.value;
+    var n1 = 0;
+    var n2 = 1;
+    var n3 = 1;
+    var temp;
+    var array = []
+    for(i = 0; i < iteration; i++){
+        array.push(n1);
+        temp = n1+n2+n3;
+        n1 = n2;
+        n2 = n3;
+        n3 = temp;
+    };
+    output.innerHTML = array;
+}
